6ff1c78cfa76507ca7f45161534c49f3308b6bd9
Onur Güler <guleronurosman@gmail.com>
2020-03-29 00:24:00 +0300

    Photo model

--- photos/models.py
+++ photos/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+from faketagram.models import TimeStampedModel
+
+
+class Photo(TimeStampedModel):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    caption = models.TextField(blank=True)
+    image = models.ImageField(upload_to='photos/%Y/%m/%d')
+
+    def __str__(self):
+        return f'{self.user.username} - {self.created_at}'
