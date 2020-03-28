304a4060044082e8cf719efa8bb30eac08bd8844
Onur Güler <guleronurosman@gmail.com>
2020-03-29 00:18:42 +0300

    Create photos app

--- faketagram/urls.py
+++ faketagram/urls.py
@@ -19,7 +19,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path('', include('core.urls', namespace='core')),
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls', namespace='accounts')),
-    path('', include('core.urls', namespace='core')),
+    path('photos/', include('photos.urls', namespace='photos')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
