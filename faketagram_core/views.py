836f1663a9e81d3668192c4d5c37bb6f896045dd
Onur Osman Güler <guleronurosman@gmail.com>
2020-04-04 23:23:04 +0300

    AJAX home feed

--- faketagram_core/views.py
+++ faketagram_core/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
-from faketagram_photos.views import get_feed
+from faketagram_photos.views import handle_ajax_home_feed
 
 
 @login_required
 def index_view(request):
-    feed = get_feed(request.user)
-    return render(request, 'core/index.html', context={'feed': feed})
+    if request.is_ajax():
+        return handle_ajax_home_feed(request)
+    return render(request, 'core/index.html')
--- faketagram_photos/views.py
+++ faketagram_photos/views.py
@@ -3,6 +3,8 @@ from django.contrib.auth.decorators import login_required
 from django.views.generic import DetailView
 from django.utils import timezone
 from django.template.loader import render_to_string
+from django.core.paginator import Paginator
+from django.http import JsonResponse
 
 from faketagram_photos.forms import PhotoAddForm, PhotoCreateStyleForm, PhotoCreateDetailForm
 from faketagram_photos.models import Photo, Like
@@ -10,7 +12,7 @@ from faketagram_photos.models import Photo, Like
 
 @login_required
 def add_view(request):
-    # TODO: published=False data oluşturmadan resmi
+    # @TODO: published=False data oluşturmadan resmi
     # session'da saklayarak fotoğraf yükleme adımlarını yap
     photo = Photo.objects.get_or_create(
         user=request.user, published=False)[0]
@@ -148,9 +150,31 @@ def get_feed(user):
     photos = Photo.objects.filter(
         user__followers__follower=user, published=True).order_by('-published_at')
 
-    user_likes = []
-    for like in user.likes.all():
-        user_likes.append(like.photo.pk)
+    # @TODO: photonun 0 like alması durumunda bir bak
+    user_likes = user.likes.filter(
+        photo__in=photos).values_list('photo', flat=True)
 
     return render_to_string(
         'photos/feed.html', context={'photos': photos, 'user_likes': user_likes})
+
+
+@login_required
+def handle_ajax_home_feed(request):
+    photos_list = Photo.objects.filter(
+        user__followers__follower=request.user, published=True).order_by('-published_at')
+
+    paginator = Paginator(photos_list, 12)  # Show 10 photos per page
+    page_number = request.GET.get('page', 1)
+    photos = paginator.get_page(page_number)
+
+    # @TODO: photonun 0 like alması durumunda bir bak
+    user_likes = request.user.likes.filter(
+        photo__in=photos).values_list('photo', flat=True)
+
+    html = render_to_string(
+        'photos/feed.html', context={'photos': photos, 'user_likes': user_likes})
+
+    if int(page_number) > paginator.num_pages:
+        html = ''
+
+    return JsonResponse({'html': html})
